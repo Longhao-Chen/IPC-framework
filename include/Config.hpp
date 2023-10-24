b6b6b2479a2bd3b66d77f514331e3fc5336c1814
Longhao.Chen <Longhao.Chen@outlook.com>
2023-10-24 11:09:32 +0800

    feat: 添加 Config

---
+++ include/Config.hpp
@@ -0,0 +1,43 @@
+/*
+ * @Author: Longhao.Chen <Longhao.Chen@outlook.com>
+ * @Date: 2023-10-21 01:07:43
+ * @LastEditors: Longhao.Chen <Longhao.Chen@outlook.com>
+ * @FilePath: /IPC-framework/include/Config.hpp
+ * @Description: 配置对象
+ * Copyright (c) 2023 by Longhao.Chen, All Rights Reserved. 
+ */
+#ifndef HEADER_CONFIG
+#define HEADER_CONFIG
+#include <fstream>
+#include <vector>
+namespace Config
+{
+const std::vector<std::string> ConfigOption = { "BackEnd" };
+
+class Config {
+    private:
+	std::string BackEnd;
+
+    public:
+	// 生成默认配置
+	Config();
+	// 从文件流中生成
+	Config(std::istream &);
+	// 保存到文件
+	void save(std::ostream &) const;
+	// 从文件或标准输入读取配置
+	friend std::istream &operator>>(std::istream &, Config &);
+	// 输出配置或保存到文件
+	friend std::ostream &operator<<(std::ostream &, const Config &);
+
+	// 获取属性
+	// 此函数只能用于获得属性，这样做的目的是防止配置被意外的更改
+	std::string operator[](const std::string co) const;
+
+	// 设置属性
+	void set(const std::string co, const std::string value);
+};
+std::istream &operator>>(std::istream &, Config &);
+std::ostream &operator<<(std::ostream &, const Config &);
+}
+#endif
---
+++ utils/Config.cpp
@@ -0,0 +1,63 @@
+/*
+ * @Author: Longhao.Chen <Longhao.Chen@outlook.com>
+ * @Date: 2023-10-21 01:26:06
+ * @LastEditors: Longhao.Chen <Longhao.Chen@outlook.com>
+ * @FilePath: /IPC-framework/utils/Config.cpp
+ * @Description: 配置对象
+ * Copyright (c) 2023 by Longhao.Chen, All Rights Reserved. 
+ */
+#include <Config.hpp>
+#include <ERROR.hpp>
+#include <string>
+
+Config::Config::Config()
+{
+	// 默认配置
+	this->BackEnd = "UDS";
+}
+
+Config::Config::Config(std::istream &ins)
+	: Config()
+{
+	ins >> *this;
+}
+
+void Config::Config::save(std::ostream &outs) const
+{
+	outs << *this;
+}
+
+std::string Config::Config::operator[](const std::string co) const
+{
+	if (co == "BackEnd")
+		return this->BackEnd;
+	else
+		throw ERROR("参数错误");
+}
+
+void Config::Config::set(const std::string type, const std::string value)
+{
+	if (type == "BackEnd")
+		this->BackEnd = value;
+	else
+		throw ERROR("参数错误");
+}
+
+std::istream &Config::operator>>(std::istream &in, Config &conf)
+{
+	std::string type, val;
+	while (in >> type) {
+		in >> val;
+		conf.set(type, val);
+	}
+	return in;
+}
+
+std::ostream &Config::operator<<(std::ostream &out, const Config &conf)
+{
+	for (auto it = ConfigOption.begin(); it == ConfigOption.end(); ++it) {
+		out << *it << " ";
+		out << conf[*it] << std::endl;
+	}
+    return out;
+}
